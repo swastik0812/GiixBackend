d2c18692cc763b41f0c5814d600598b62c21f930
swastik <swastikpal0812@gmail.com>
2020-07-06 17:28:40 +0530

    some changes after 2 hours in backend now insert the data inside db

--- src/index.js
+++ src/index.js
@@ -2,71 +2,17 @@ const express = require("express");
 const con = require("./db/sql");
 const upload = require("express-fileupload");
 const app = express();
+const userRouter = require("./router/user");
 app.use(upload());
 const bodyparser = require("body-parser");
 
 const port = process.env.PORT || 3003;
-
 app.use(bodyparser.json());
-
+app.use(userRouter);
 app.listen(port, () => {
   console.log("server is open on port" + port);
 });
 
-// app.get("/user", (res, req) => {
-//   con.query("SELECT * FROM test.student WHERE s_id=1 ", function (
-//     err,
-//     result,
-//     fields
-//   ) {
-//     if (err) throw err;
-//     console.log(result);
-//     console.log("hello");
-//   });
-
-//   console.log("hey baby");
-// });
-
 app.get("/", (req, res) => {
   res.sendFile(__dirname + "/index.html");
-});
-
-app.post("/", (req, res) => {
-  const fileName = "swastik";
-  if (req.files) {
-    console.log(req.files);
-    const file = req.files.file;
-    const filename = "swastik";
-    console.log(filename);
-
-    file.mv("./uploads/" + filename, function (err) {
-      if (err) {
-        res.send(err);
-      } else {
-        res.send("file Uploaded");
-      }
-    });
-  }
-
-  const date = new Date().toLocaleString("en-US", {
-    timeZone: "Asia/Kolkata",
-  });
-  console.log(date);
-  //   /home/aiksstw /
-  //     work /
-  //     nodeagain /
-  //     node_app /
-  //     node_with_sql /
-  //     src /
-  //     uploads /
-  //     Swastik_pal_Resume_u.d(1).docx;
-
-  let stmt = `INSERT INTO test.Giix(FileName, path, uploadedOn)
-            VALUES(?,?,?)`;
-  let todo = [fileName, "swas", date];
-  con.query(stmt, todo, function (err, result, fields) {
-    if (err) throw err;
-    console.log(result[0].roll_no);
-    console.log("hello");
-  });
 });
--- src/router/user.js
+++ src/router/user.js
@@ -1,3 +1,47 @@
 const express = require("express");
+const con = require("../db/sql");
 
+const router = express.Router();
 
+router.get("/files", (req, res) => {
+  con.query("SELECT * FROM test.userFile ", function (err, result, fields) {
+    if (err) throw err;
+    console.log(result);
+    console.log("hello");
+  });
+});
+
+router.post("/", (req, res) => {
+  let fileName = "";
+  const date = new Date().toLocaleString("en-US", {
+    timeZone: "Asia/Kolkata",
+  });
+  date.toString();
+  let path = "/home/work/nodeagain/node_app/node_with_sql/src/uploads/";
+
+  if (req.files) {
+    console.log(req.files);
+    const file = req.files.file;
+    fileName = file.name;
+    console.log(fileName);
+
+    file.mv("./uploads/" + fileName, function (err) {
+      if (err) {
+        res.send(err);
+      } else {
+        res.send("file Uploaded");
+      }
+    });
+  }
+  const finalPath = path.concat(fileName);
+  let stmt = `INSERT INTO test.userFile(path, FileName, uploadedOn)
+              VALUES(?,?,?)`;
+  let todo = [finalPath, fileName, date];
+  con.query(stmt, todo, function (err, result, fields) {
+    if (err) throw err;
+    console.log(result[0]);
+    console.log("hello");
+  });
+});
+
+module.exports = router;
