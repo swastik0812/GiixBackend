a0fa7300ad75154c16554e1720597b639a56bafb
swastik <swastikpal0812@gmail.com>
2020-07-07 04:45:30 +0530

    completed

--- src/index.js
+++ src/index.js
@@ -1,9 +1,13 @@
 const express = require("express");
 const con = require("./db/sql");
-const upload = require("express-fileupload");
 const app = express();
 const userRouter = require("./router/user");
-app.use(upload());
+var cors = require("cors");
+app.use(cors());
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
 const bodyparser = require("body-parser");
 
 const port = process.env.PORT || 3003;
--- src/router/user.js
+++ src/router/user.js
@@ -1,47 +1,58 @@
 const express = require("express");
 const con = require("../db/sql");
+const multer = require("multer");
 
 const router = express.Router();
 
 router.get("/files", (req, res) => {
-  con.query("SELECT * FROM test.userFile ", function (err, result, fields) {
-    if (err) throw err;
-    console.log(result);
-    console.log("hello");
-  });
+  try {
+    let response = [];
+    con.query("SELECT * FROM test.Giix ", function (err, result, fields) {
+      if (err) throw err;
+      const dataArray = Object.keys(result).map((data) => {
+        return result[data];
+      });
+      for (let i = dataArray.length < 10 ? dataArray.length : 10; i >= 1; i--) {
+        response.push(dataArray[i]);
+      }
+      res.send(response);
+    });
+  } catch (e) {
+    res.send(e);
+  }
 });
 
-router.post("/", (req, res) => {
-  let fileName = "";
-  const date = new Date().toLocaleString("en-US", {
-    timeZone: "Asia/Kolkata",
-  });
-  date.toString();
+const upload = multer({
+  dest: "uploads",
+});
+
+router.post("/", upload.single("uploads"), (req, res) => {
+  let fileName = req.file.filename;
+  const date = new Date();
   let path = "/home/work/nodeagain/node_app/node_with_sql/src/uploads/";
-
-  if (req.files) {
-    console.log(req.files);
-    const file = req.files.file;
-    fileName = file.name;
-    console.log(fileName);
-
-    file.mv("./uploads/" + fileName, function (err) {
-      if (err) {
-        res.send(err);
-      } else {
-        res.send("file Uploaded");
-      }
+  filename = req.file.filename;
+  if (req.file) {
+    const finalPath = path.concat(fileName);
+    let stmt = `INSERT INTO test.Giix(path, FileName, uploadedOn)
+              VALUES(?,?,?)`;
+    let todo = [finalPath, fileName, date];
+    con.query(stmt, todo, function (err, result, fields) {
+      if (err) throw err;
     });
   }
-  const finalPath = path.concat(fileName);
-  let stmt = `INSERT INTO test.userFile(path, FileName, uploadedOn)
-              VALUES(?,?,?)`;
-  let todo = [finalPath, fileName, date];
-  con.query(stmt, todo, function (err, result, fields) {
-    if (err) throw err;
-    console.log(result[0]);
-    console.log("hello");
-  });
+  res.send();
+});
+
+router.post("/updateName", async (req, res) => {
+  let data = [req.body.name, req.body.id];
+  await con.query(
+    "UPDATE test.Giix SET name=? WHERE _id=?",
+    data,
+    (err, result, fields) => {
+      if (err) throw err;
+      res.send(result);
+    }
+  );
 });
 
 module.exports = router;
